464ae6206754ebfa842d2558a0fbc8daaabd092e
HongJunPyo0222 <widejunpyo@gmail.com>
2026-04-22 19:41:37 +0900

    feat: Seat DTO 클래스 생성 (30석 배열 구조)

---
+++ src/DTO/Seat.java
@@ -0,0 +1,23 @@
+package DTO;
+
+public class Seat {
+    //false: 빈 좌석, true: 예약됨
+    private boolean[] seatStatus;
+
+    public Seat() {
+        this.seatStatus = new boolean[30];
+    }
+
+    public boolean[] getSeatStatus() {
+        return seatStatus;
+    }
+
+    // 특정 좌석의 예약 여부 확인
+    public boolean isBooked(int seatNumber) {
+        return seatStatus[seatNumber - 1];
+    }
+
+    public void setBooked(int seatNumber, boolean status) {
+        this.seatStatus[seatNumber - 1] = status;
+    }
+}
