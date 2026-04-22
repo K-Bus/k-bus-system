7ac8027cd9e68f5538c13411ca4d66c8a469574f
woodgeon <ad4086@naver.com>
2026-04-22 16:45:33 +0900

    init: 초기 프로젝트 세팅

---
+++ src/DTO/User.java
@@ -0,0 +1,13 @@
+package DTO;
+
+public class User {
+    private String Email;
+
+    public User(String email) {
+        this.Email = email;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+}
