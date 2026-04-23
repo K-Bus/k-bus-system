a8cb19b61a1fc6cc5b8284f0e85132e63ed88882
jaewoolee <ljw5953@gmail.com>
2026-04-23 16:26:11 +0900

    예약내역 읽기(readReservation) 예약내역 쓰기(writeReservation) 추가

--- src/Service/FileIOService.java
+++ src/Service/FileIOService.java
@@ -1,29 +1,31 @@
-package Service;
+﻿package Service;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import DTO.Reservation;
 import DTO.Schedule;
 
-class FileIOService {
-    private final BufferedReader CityBufferedReader;
-    private final BufferedReader ScheduleBufferedReader;
-    
-    FileIOService() throws IOException {
-    	//절대경로로 박으면 배포 시 환경 다름으로 인한 예외 방지
-		CityBufferedReader = Files.newBufferedReader(Paths.get("data", "cities.txt"), StandardCharsets.UTF_8);
-		ScheduleBufferedReader = Files.newBufferedReader(Paths.get("data", "schedule.txt"), StandardCharsets.UTF_8);
-    }
-	
-    //cities.txt : 평문 읽기
-	Map<Integer, String> readCities(){
+public class FileIOService {
+
+    //평문 읽기 : cities.txt
+	public Map<Integer, String> readCities() throws IOException{
+		BufferedReader CityBufferedReader = Files.newBufferedReader(Paths.get("data", "cities.txt"), StandardCharsets.UTF_8);
 		Map<Integer, String> citiesMap = new HashMap<>();
 		
         try {
@@ -36,25 +38,88 @@ class FileIOService {
         } catch (IOException e) {
 			e.printStackTrace();
 			return null;
+        } finally {
+        	CityBufferedReader.close();
         }
 	}
 	
-	//schedule.txt : 평문 읽기
-	ArrayList<Schedule> readScehdule(){
-		ArrayList<Schedule> schedules = new ArrayList<>();
-		
+	//평뮨 읽기 : schedule.txt
+	public List<Schedule> readScehdule() throws IOException{
+		BufferedReader ScheduleBufferedReader = Files.newBufferedReader(Paths.get("data", "schedule.txt"), StandardCharsets.UTF_8);
+		List<Schedule> schedules = new ArrayList<>();
+	
         try {
         	String line;
 			while ((line = ScheduleBufferedReader.readLine()) != null) {
 				String[] row = line.split(" ");
-			    //citiesMap.put(Integer.parseInt(row[0]), row[1]);
 				schedules.add(new Schedule (Integer.parseInt(row[0]), Integer.parseInt(row[1]), Integer.parseInt(row[2].substring(0, 2)), row[3]));
 			}
 			return schedules;
         } catch (IOException e) {
 			e.printStackTrace();
 			return null;
+        } finally {
+        	ScheduleBufferedReader.close();
         }
+	}
+	
+	//역직렬화 읽기 : reservation.txt
+	public List<Reservation> readReservation(){
+		File file = new File("data/reservation.txt");
+		List<Reservation> reservations = new ArrayList<>();
+		
+		try (FileInputStream fis = new FileInputStream(file); ObjectInputStream ois = new ObjectInputStream(fis);){
+			while (true) {
+	            try {
+	            	reservations.add((Reservation) ois.readObject());
+	            } catch(ClassNotFoundException e) {
+	            	e.printStackTrace();
+	            } catch (EOFException e) {
+	            	break;
+	            }
+	        }
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return reservations;
+	}
+	
+	//직렬화 쓰기 : reservation.txt
+	void writeReservation(Reservation reservation) {
+	    File file = new File("data/reservation.txt");
+
+	    try {
+	        ObjectOutputStream oos;
+
+	        if (file.exists() && file.length() > 0) {
+	            // append
+	            oos = new AppendableObjectOutputStream(
+	                    new FileOutputStream(file, true));
+	        } else {
+	            // 처음 생성 → 헤더 필요
+	            oos = new ObjectOutputStream(
+	                    new FileOutputStream(file));
+	        }
+
+	        oos.writeObject(reservation);
+	        oos.close();
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	//writeReservation()에서 사용되는 inner class : ObjectOutputStream은 append가 그냥 안됨(처음 생성시 스트림헤더를 파일에 쓰기 때문)
+	private class AppendableObjectOutputStream extends ObjectOutputStream {
+	    public AppendableObjectOutputStream(OutputStream out) throws IOException {
+	        super(out);
+	    }
+
+	    @Override
+	    protected void writeStreamHeader() throws IOException {
+	        reset(); // 헤더 안 씀
+	    }
 	}
 	
 }
