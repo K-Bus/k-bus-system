753b4ac95bcb55d8d9cf54d789f36a00d9c921b8
nunomi0 <ykys1@naver.com>
2026-04-23 14:06:37 +0900

    refactor(reservation): 미사용 필드 삭제(id, date)

--- src/DTO/Reservation.java
+++ src/DTO/Reservation.java
@@ -1,27 +1,15 @@
 package DTO;
 
 public class Reservation {
-	private int id;
 	private String userId;
 	private int busRouteId;
-	private String date;
 	
 	public Reservation() {}
 	
-	private Reservation(int id, String userId, int busRouteId, String date) {
-		this.id = id;
+	private Reservation(String userId, int busRouteId) {
         this.userId = userId;
         this.busRouteId = busRouteId;
-        this.date = date;
     }
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getUserId() {
 		return userId;
@@ -37,13 +25,5 @@ public class Reservation {
 
 	public void setBusRouteId(int busRouteId) {
 		this.busRouteId = busRouteId;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
 	}
 }
