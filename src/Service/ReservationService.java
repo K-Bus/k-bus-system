edab18461fa2897952d56c53907e64e07967ab4a
woodgeon <ad4086@naver.com>
2026-04-24 09:16:03 +0900

    feat: Reservation List 반환 기능 추가

--- src/Service/ReservationService.java
+++ src/Service/ReservationService.java
@@ -1,14 +1,13 @@
 package Service;
 
 import DTO.Reservation;
-import DTO.BusRoute;
 import DTO.Seat;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ReservationService {
+public class ReservationService implements Serializable {
 	
 	private List<Reservation> reservationList;
 	private FileIOService fileIOService;
@@ -18,55 +17,58 @@ public class ReservationService {
 		this.fileIOService=fileIOService;
 	}
 	
-	public void makeReservation(String userId, String date, int busRouteId, int selectedSeatNumber) {
-		
-		// 에약이 안되는 경우		
-        try {
-			if (searchBusRouteSeat(date, busRouteId).isBooked(selectedSeatNumber)) {
-				System.out.println("이미 예약된 좌석입니다.");
-				return;
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// 예약 생성하기
-        
-		SearchService.searchBusRouteSeat(date, busRouteId, selectedSeatNumber);
-		Reservation reservation = new Reservation(userId, date, busRouteId, selectedSeatNumber);
-		reservationList.add(reservation);
-        // fileIOService.writeReservation(reservationList);
-		
-		System.out.println(userId + "님 예약 완료되었습니다.");
-		printReservationDetail(reservation);
-		System.out.println();
-	    
+	public void makeReservation(String userId, String date, int busRouteId, int seatNumber) {
+
+        Reservation reservation = new Reservation(userId, date, busRouteId, seatNumber);
+
+        // 현재 들고있는 메모리에 추가하고
+        reservationList.add(reservation);
+
+        // 파일 저장
+        fileIOService.writeReservation(reservation);
 	}
 
 	public void checkReservation(String userId) {
 		List<Reservation> result = reservationList.stream()
 								.filter(r->r.getUserId().equals(userId))
 								.collect(Collectors.toList());
-		
+        if (result.isEmpty()) {
+            System.out.println("예약 내역이 없습니다.");
+            return;
+        }
+
 		System.out.println("예약 정보입니다.");
 		for (Reservation reservation : result) {
 			printReservationDetail(reservation);
-		}		
-		System.out.println();
+		}
 	}
-	
-	public void cancelReservation(Reservation reservation) {
-		reservationList.remove(reservation);
-		// fileIOService.writeReservation(reservationList);
-		
-		System.out.println("예약을 취소했습니다.");
-		printReservationDetail(reservation);
-		System.out.println();
-	}
+
+    public void cancelReservation(Reservation reservation, List<Seat> seatList) {
+        // 1. 예약 삭제
+        reservationList.remove(reservation);
+        // 2. Seat 반영 (좌석 다시 열기)
+        for (Seat seat : seatList) {
+            if (seat.getDate().equals(reservation.getDate())
+                    && seat.getBusId() == reservation.getBusRouteId()) {
+
+                seat.setBooked(reservation.getSeatNumber(), false);
+                break;
+            }
+        }
+        // 3. 파일 저장
+        fileIOService.writeReservations(reservationList);
+
+        System.out.println("예약을 취소했습니다.");
+        printReservationDetail(reservation);
+    }
 	
 	public void printReservationDetail(Reservation reservation) {
-		// TODO: 예약 내역 출력 구현
-		System.out.printf("[%s->%s] %s %s번 버스 %d번 좌석\n", "대구", "부산", reservation.getDate(), reservation.getBusRouteId(), reservation.getSeatNumber());
+        System.out.printf("[%s] %s %d번 버스 %d번 좌석\n", "버스", reservation.getDate(), reservation.getBusRouteId(), reservation.getSeatNumber());
 	}
+
+    public List<Reservation> findByUserId(String userId) {
+        return reservationList.stream()
+                .filter(r -> r.getUserId().equals(userId))
+                .collect(Collectors.toList());
+    }
 }
